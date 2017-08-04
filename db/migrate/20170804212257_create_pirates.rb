82459397eaa6bf1931bb5e2bb247e88ec916ac6d
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-04 21:37:06 +0000

    Getting databases up and running

--- db/migrate/20170804212257_create_pirates.rb
+++ db/migrate/20170804212257_create_pirates.rb
@@ -1,4 +1,10 @@
 class CreatePirates < ActiveRecord::Migration[5.1]
   def change
+    creatle_table :pirates do |t|
+      t.string :name
+      t.string :weight
+      t.string :height
+    end
   end
+
 end
