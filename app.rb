1965fb16dcac979ff3b2eb14d8665098c071f225
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-05 14:10:40 +0000

    Got config.ru up and running

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ module FormsLab
   class App < Sinatra::Base
 
     # /pirates 'gets' all the pirates.  This is the convention (ie: /puppies)
-    get '/pirates'
-    @pirates = Pirate.all
-    erb :index
+    get '/pirates' do
+      @pirates = Pirate.all
+      erb :'pirates/index'
+      # Nows, at this point, test your route.
+    end
+
 
   end
-
 end
