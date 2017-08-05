a71afd89b964c59e17bd5c8433395ca0e6d95744
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-05 14:28:52 +0000

    Added some commented notes.

--- app.rb
+++ app.rb
@@ -11,6 +11,11 @@ module FormsLab
       # Nows, at this point, test your route.
     end
 
+    get '/pirates/:id' do 
+      #Good place to pause.  She throws in a binding.pry 
+      # and examines the convention /:id in depth!!!
+    end 
+
 
   end
 end
